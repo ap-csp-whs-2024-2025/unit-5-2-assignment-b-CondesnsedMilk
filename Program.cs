9bc704d5d629b5ea248bc2b5c502753a36bbb73a
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-01-10 21:58:52 +0000

    Hi

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace unit_5_2_assignment_b_CondesnsedMilk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var names = new List<string>{"Peter", "Bruce", "Steve", "Tony", "Natasha", "Clint", "Wanda", "Hope", "Danny", "Carol"};
+        var numbers = new List<int>{100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80};
+
+        int count = 0;
+
+        while (count > names.Count())
+        {
+            if (names.ElementAt<> )
+            count++;
+        }
+    }
+}
